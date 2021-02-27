07bbeb18be5fcbd05de7f7909ca05362af58c997
Soham <68921363+sohxm7@users.noreply.github.com>
2021-02-28 00:53:19 +0530

    Update liwe.py

--- liwe.py
+++ liwe.py
@@ -1,5 +1,5 @@
-from PIL import Image
 import os
+from PIL import Image
 from datetime import datetime
 
 dixt = {"cliff":"images/The-Cliffs/",
@@ -7,23 +7,22 @@ dixt = {"cliff":"images/The-Cliffs/",
         "desert":"images/The-Desert/",
         "lake":"images/The-Lake/"
         }
+
+# For selecting the wallpaper edit line below
+# wallpapers: "cliff", "beach", "desert", "lake"
 selected = dixt["cliff"]
 
 nowx = datetime.now()
 hour = int(nowx.strftime("%H"))
 minx = int(nowx.strftime("%M"))
-
 imgx = int(hour/3) + 1
 alpx = (((hour%3)*60)+minx)/180
-
 if imgx==8:
         nexx=0
 else:
         nexx = imgx + 1
-        
 bg = Image.open(selected+str(imgx)+".jpg")
 fg = Image.open(selected+str(nexx)+".jpg")
 Image.blend(bg, fg, alpx).save("out.png")
-
 command = "feh --bg-scale out.png"
 os.system(command)
