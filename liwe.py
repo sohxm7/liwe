c5e78dac0f18307569f70d242484507a624c8819
Soham <68921363+sohxm7@users.noreply.github.com>
2021-02-28 00:21:47 +0530

    Update liwe.py

--- liwe.py
+++ liwe.py
@@ -1,13 +1,14 @@
 from PIL import Image
 import os
 from datetime import datetime
+
 dixt = {"cliff":"images/The-Cliffs/",
         "beach":"images/The-Beach/",
         "desert":"images/The-Desert/",
         "lake":"images/The-Lake/"
         }
-
 selected = dixt["cliff"]
+
 nowx = datetime.now()
 hour = int(nowx.strftime("%H"))
 minx = int(nowx.strftime("%M"))
@@ -15,8 +16,14 @@ minx = int(nowx.strftime("%M"))
 imgx = int(hour/3) + 1
 alpx = (((hour%3)*60)+minx)/180
 
-bg = Image.open(selected+str(imgx-1)+".jpg")
-fg = Image.open(selected+str(imgx)+".jpg")
+if imgx==8:
+        nexx=0
+else:
+        nexx = imgx + 1
+        
+bg = Image.open(selected+str(imgx)+".jpg")
+fg = Image.open(selected+str(nexx)+".jpg")
 Image.blend(bg, fg, alpx).save("out.png")
-comandx = "feh --bg-scale out.png"
-os.system(comandx)
+
+command = "feh --bg-scale out.png"
+os.system(command)
