61848208a2e7b5a4e2b5e637b606a4ffda940e9d
Soham <68921363+sohxm7@users.noreply.github.com>
2021-02-28 19:33:24 +0530

    Update liwe.py

--- liwe.py
+++ liwe.py
@@ -1,29 +1,39 @@
+#!/usr/bin/env python
+
 import os
 from PIL import Image
 from datetime import datetime
 
-dixt = {"cliff":"images/c",
-        "beach":"images/b",
-        "desert":"images/d",
-        "lake":"images/l"
+dixt = {"cliff": "/home/soham/liwe/images/c",
+        "beach": "/home/soham/liwe/images/b",
+        "desert":"/home/soham/liwe/images/d",
+        "lake":  "/home/soham/liwe/images/l"
         }
 
 # For selecting the wallpaper edit line below
 # wallpapers: "cliff", "beach", "desert", "lake"
-
 selected = dixt["cliff"]
 
+# Take current Time
 nowx = datetime.now()
 hour = int(nowx.strftime("%H"))
 minx = int(nowx.strftime("%M"))
+
+# Divide 24 hours into 8 equal parts
+# Determine the alpha of next image
 imgx = int(hour/3) + 1
 alpx = (((hour%3)*60)+minx)/180
 if imgx==8:
         nexx=0
 else:
         nexx = imgx + 1
+
+# Merge both images based on time and alpha
 bg = Image.open(selected+str(imgx)+".png")
 fg = Image.open(selected+str(nexx)+".png")
 Image.blend(bg, fg, alpx).save("out.png")
+
+# Command for changing the wallpaper
+# Can customise it accordingly
 command = "feh --bg-scale out.png"
 os.system(command)
