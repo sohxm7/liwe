cd070bd26fd5d913b223bd5bd205fa6d2b18c258
Soham <68921363+sohxm7@users.noreply.github.com>
2021-03-04 22:58:09 +0530

    Update init.py

--- init.py
+++ init.py
@@ -1,17 +1,34 @@
 from PIL import Image
 from screeninfo import get_monitors
+
 for m in get_monitors():
     wid = str(m).split()[2][-5:-1]
     hei = str(m).split()[3][-5:-1]
 
-print("Starting Screen Size Optimizations...")
+print("wait a sec...")
+
 wid = int(wid)
 hei = int(hei)
-size = wid, hei
+
+size = wid, wid
+
+print("\n")
+
 for i in range(1, 9):
-    print(i)
+    print("Scaling [",i," / 8 ]")
     for j in ["b","c","d","l"]:
         im = Image.open("images/"+j+str(i)+".jpg")
         im_resized = im.resize(size, Image.ANTIALIAS)
-        im_resized.save("images/"+j+str(i)+".png", "PNG")
-print("Done Successfully...")
+        im_resized.save("images/new/"+j+str(i)+".png", "PNG")
+
+print("\n")
+
+x = int((wid-hei)/2)
+for i in range(1, 9):
+    print("Cropping [",i," / 8 ]")
+    for j in ["b","c","d","l"]:
+        im = Image.open("images/new/"+j+str(i)+".png")
+        im_resized = im.crop((0, x, wid, x+hei))
+        im_resized.save("images/new/"+j+str(i)+".png", "PNG")
+
+print("\nDone Successfully...")
