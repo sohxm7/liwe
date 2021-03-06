d39a67ce4588234b8e2b8f06051042f66f92f957
Soham <68921363+sohxm7@users.noreply.github.com>
2021-03-06 10:06:11 +0530

    Update init.py

--- init.py
+++ init.py
@@ -1,5 +1,8 @@
+import os
 from PIL import Image
 from screeninfo import get_monitors
+
+path = os.getcwd()
 
 for m in get_monitors():
     wid = str(m).split()[2][-5:-1]
@@ -12,23 +15,23 @@ hei = int(hei)
 
 size = wid, wid
 
-print("\n")
+print("")
 
 for i in range(1, 9):
-    print("Scaling [",i," / 8 ]")
+    print("Scaling [",i,"/ 8 ]")
     for j in ["b","c","d","l"]:
-        im = Image.open("images/"+j+str(i)+".jpg")
+        im = Image.open(path+"/images/"+j+str(i)+".jpg")
         im_resized = im.resize(size, Image.ANTIALIAS)
-        im_resized.save("images/new/"+j+str(i)+".png", "PNG")
+        im_resized.save(path+"/images/new/"+j+str(i)+".png", "PNG")
 
-print("\n")
+print("")
 
 x = int((wid-hei)/2)
 for i in range(1, 9):
-    print("Cropping [",i," / 8 ]")
+    print("Cropping [",i,"/ 8 ]")
     for j in ["b","c","d","l"]:
-        im = Image.open("images/new/"+j+str(i)+".png")
+        im = Image.open(path+"/images/new/"+j+str(i)+".png")
         im_resized = im.crop((0, x, wid, x+hei))
-        im_resized.save("images/new/"+j+str(i)+".png", "PNG")
+        im_resized.save(path+"/images/new/"+j+str(i)+".png", "PNG")
 
 print("\nDone Successfully...")
