caf7cbac65cf9539cc639847e54f7c7fe455751a
Soham <68921363+sohxm7@users.noreply.github.com>
2021-03-10 22:50:20 +0530

    Update init.py

--- init.py
+++ init.py
@@ -15,23 +15,23 @@ hei = int(hei)
 
 size = wid, wid
 
-print("")
-
 for i in range(1, 9):
-    print("Scaling [",i,"/ 8 ]")
+    outx = "Scaling  ["+str(i)+"/8]"
+    print(outx)
     for j in ["b","c","d","l"]:
-        im = Image.open(path+"/liwe/images/"+j+str(i)+".jpg")
+        im = Image.open(path+"/images/"+j+str(i)+".jpg")
         im_resized = im.resize(size, Image.ANTIALIAS)
-        im_resized.save(path+"/liwe/images/new/"+j+str(i)+".png", "PNG")
+        im_resized.save(path+"/images/new/"+j+str(i)+".png", "PNG")
 
-print("\n")
+print("")
 
 x = int((wid-hei)/2)
 for i in range(1, 9):
-    print("Cropping [",i,"/ 8 ]")
+    outx = "Cropping ["+str(i)+"/8]"
+    print(outx)
     for j in ["b","c","d","l"]:
-        im = Image.open(path+"/liwe/images/new/"+j+str(i)+".png")
+        im = Image.open(path+"/images/new/"+j+str(i)+".png")
         im_resized = im.crop((0, x, wid, x+hei))
-        im_resized.save(path+"/liwe/images/new/"+j+str(i)+".png", "PNG")
+        im_resized.save(path+"/images/new/"+j+str(i)+".png", "PNG")
 
 print("\nDone Successfully...")
